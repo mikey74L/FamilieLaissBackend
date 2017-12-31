bd154248b0091c6d0fc9b10a1cc04ba523c0c672
Michael Laiß <mikey74@hotmail.de>
2017-12-31 02:35:38 +0100

    Erste Schritte für das Einbinden des Material Dashboard Themes (CSS). Erste Schritte mit Login-Page

---
+++ FamilieLaissCoreHelpers/TagHelpers/BoldTagHelper.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilieLaissCoreHelpers.TagHelpers
+{
+    [HtmlTargetElement(Attributes = "bold")]
+    public class BoldTagHelper : Microsoft.AspNetCore.Razor.TagHelpers.TagHelper
+    {
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.RemoveAll("bold");
+            output.PreContent.SetHtmlContent("<strong>");
+            output.PostContent.SetHtmlContent("</strong>");
+        }
+    }
+}
