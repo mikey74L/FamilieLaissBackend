0aa0589da307582c62021b3b4d32aa2c4b04fe85
Michael Laiß <mikey74@hotmail.de>
2018-01-29 09:01:03 +0100

    Zwischenstand

---
+++ FamilieLaissIdentity/wwwroot/js/Views/Account/Login.js
@@ -0,0 +1,7 @@
+﻿jQuery(document).ready(function () {
+    //Wenn auf den Link für den Login geklickt wird
+    //wird der nachfolgende Code ausgeführt
+    $('#link_Login').on('click', function () {
+        alert('Das ist ein Test');
+    });
+});
